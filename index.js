5dff4bfd7f73e90814ace888f097a8503bede761
Yosuf Gurung <yosufgurung@gmail.com>
2018-12-16 05:27:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   mew_object = Object.assign({}, key, value)
-  
+
 
   return new_object
 }
