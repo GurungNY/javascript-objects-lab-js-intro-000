7beda8711b6a47d78911a222c6f17a015e9c0370
Yosuf Gurung <yosufgurung@gmail.com>
2018-12-16 07:13:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  const new_obj = Object.asign({}, object)
+//  const new_obj = Object.asign({}, object)
 
-//  delete object[key]
+  delete object[key]
 
-  return new_obj
+  return object
 }
